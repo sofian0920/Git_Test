d97a271785ed1e7085b63db03e71825026b27aa8
exem <norinasofia@mail.ru>
2023-07-17 12:19:49 +0300

    Initial Commit

---
+++ Git_Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git_Test
+//
+//  Created by Софья Норина on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
