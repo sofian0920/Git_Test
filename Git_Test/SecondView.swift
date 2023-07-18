8646c16869ac6f28cf49055f587e7c5fd34161c2
exem <norinasofia@mail.ru>
2023-07-18 18:14:21 +0300

    Create SecondView

---
+++ Git_Test/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  Git_Test
+//
+//  Created by Софья Норина on 18.07.2023.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
