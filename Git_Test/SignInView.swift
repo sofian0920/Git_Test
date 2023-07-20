69e1920cc7037976bfd2ec19a62e6d18dbf9a7ab
exem <norinasofia@mail.ru>
2023-07-20 17:00:20 +0300

    Create custom TextField

--- Git_Test/SignInView.swift
+++ Git_Test/SignInView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SignInView: View {
     @State var token = ""
+    @FocusState private var editing
     var body: some View {
         ZStack{
             Color.black.ignoresSafeArea(.all)
@@ -17,19 +18,38 @@ struct SignInView: View {
                     .resizable()
                     .frame(width: 96, height: 96)
                     .padding(45)
-                TextField("Personal access token", text: $token)
-                    .textContentType(.password)
-                    .background(Color.black)
-                    .textFieldStyle(.roundedBorder)
-                    .frame(width: 343, height: 48)
-                    .foregroundColor(Color.white)
-                    .padding()
-                
+                TextField(
+                         "",
+                         text: $token,
+                         prompt: Text("Personal access token")
+                            .foregroundColor(.gray)
+                     )
+                     .frame(width: 343, height: 37)
+                     .textFieldStyle(BorderedStyle(focused: editing))
+                     .focused($editing)
+                     .multilineTextAlignment(.leading)
+                     .keyboardType(.numberPad)
+                   }
+                    
             }
         }
-           
+        
     }
-}
+
+struct BorderedStyle: TextFieldStyle {
+  var focused: Bool
+
+  func _body(configuration: TextField<Self._Label>) -> some View {
+      configuration
+      .padding(10)
+      .background(
+          Rectangle()
+              .stroke(
+                  focused ? Color.red : Color.gray, lineWidth: 2
+              )
+      )
+      .cornerRadius(8)
+  }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
