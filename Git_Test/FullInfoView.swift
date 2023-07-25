fe646f5c903f732c39f2e269fb710e8cbb8cf235
exem <norinasofia@mail.ru>
2023-07-25 17:46:14 +0300

    Add FullInfiView

---
+++ Git_Test/FullInfoView.swift
@@ -0,0 +1,26 @@
+//
+//  FullInfoView.swift
+//  Git_Test
+//
+//  Created by Софья Норина on 25.07.2023.
+//
+
+import SwiftUI
+
+struct FullInfoView: View {
+    
+    var body: some View {
+        ZStack{
+            Color.mainThim.ignoresSafeArea(.all)
+            VStack{
+                
+            }
+        }
+    }
+}
+
+struct FullInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        FullInfoView()
+    }
+}
