9be56df8a5cc2f82808fcf35c55cf764a785aa13
exem <norinasofia@mail.ru>
2023-08-03 17:38:05 +0300

    Add struct Repositories , create func for work with API

---
+++ Git_Test/GitHubAPI.swift
@@ -0,0 +1,38 @@
+//
+//  GitHubAPI.swift
+//  Git_Test
+//
+//  Created by Софья Норина on 3.08.2023.
+//
+
+import Foundation
+import Alamofire
+
+
+struct GitHubAPI {
+    static let accessToken =  "YOUR_ACCESS_TOKEN"
+    static let baseURL = "https://api.github.com"
+}
+
+
+func getRepositories(complition: @escaping([Repository]?) -> Void) -> Void {
+    let headers: HTTPHeaders = ["Authorization" : "token \(GitHubAPI.accessToken)"]
+    
+    AF.request("\(GitHubAPI.baseURL)/user/repos", headers: headers)
+        .responseDecodable(of: [Repository].self) { response in
+            switch response.result {
+            case .success(let repositories):
+                complition(repositories)
+            case .failure(let error):
+                print("Error: \(error)")
+                complition(nil)
+            }
+        }
+}
+
+
+struct Repository: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let description: String?
+}
